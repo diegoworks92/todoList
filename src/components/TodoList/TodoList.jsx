e42b38bbd91f3a149c89f2d9725d896a9957ed9a
Diego Reyes <diegoreyes@diegoworks.com>
2025-11-03 20:05:59 +0100

    se agregó prioridad y la modificación

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -5,19 +5,25 @@ import { Button } from "react-bootstrap";
 const TodoList = () => {
 
 const [tarea, setTarea] = useState("");
-const [lista, setLista] = useState([])
+const [lista, setLista] = useState([]);
+const [prioridad, setPrioridad] = useState("");
+
 
 const handleSubmit = (e) => {
     e.preventDefault();
-     if(tarea.trim() === "") return;
-  setLista([...lista, {id: Date.now(), texto: tarea, completada: false}]);
+     if(tarea.trim() === "" || prioridad === ""){
+      alert("Debes ingresar una tarea y una prioridad");
+      return;
+     }
+  setLista([...lista, {id: Date.now(), texto: tarea, prioridad, completada: false}]);
   setTarea("");
+  setPrioridad("");
 }
 
 const handleChange = (e) => setTarea(e.target.value)
 
 
-// Delete por id
+// delete por id
 const handleDelete = (id) => {
   setLista(lista.filter((t) => t.id !== id))
 }
@@ -31,17 +37,35 @@ const handleDelete = (id) => {
     );
   };
 
+const handleMod = (id) => {
+  const nuevoTexto = prompt("Modifica la tarea:");
+  if(!nuevoTexto) return;
+
+  setLista(lista.map((t) => t.id === id ? {...t, texto: nuevoTexto}: t))
+}
+
 
   return (
     <div className="container mt-4">
    <form onSubmit={handleSubmit} className="mb-3"> 
         <label>
             Tarea: <input type="text" value={tarea} placeholder="Escribe una tarea" name="tarea" onChange={handleChange}/>
+          <select name="prioridad" value={prioridad} onChange={(e) => setPrioridad(e.target.value)}>
+            <option value="">Selecciona prioridad</option>
+            <option value="baja">Baja</option>
+            <option value="media">Media</option>
+            <option value="alta">Alta</option>
+          </select>
         </label>
         <Button type="submit" variant="primary">Agregar</Button>
 
         <ul>
-          {lista.map((t, index) => (<li key={index}><p>{t.texto}</p><Button variant="danger" size="sm" onClick={() => handleDelete(t.id)}>Eliminar</Button> <Button variant={t.completada ? "success" : "outline-success"} onClick={() => handleToggle(t.id)}>{t.completada ? "Hecho" : "Ok?"}</Button></li>))}
+          {lista.map((t, index) => (<li key={index}><p>{t.texto}</p> <p>{t.prioridad}</p>
+          <Button variant="danger" size="sm" onClick={() => handleDelete(t.id)}>Eliminar</Button> 
+          <Button variant={t.completada ? "success" : "outline-success"} onClick={() => handleToggle(t.id)}>{t.completada ? "Hecho" : "Ok?"}</Button>
+          <Button onClick={() => handleMod(t.id)}>Modificar</Button>
+          </li>))}
+          
         </ul>
 
     </form>
