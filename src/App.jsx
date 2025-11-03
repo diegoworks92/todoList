ae0989a04e466785b0c9b46d03dd0c9e5ea07713
Diego Reyes <diegoreyes@diegoworks.com>
2025-11-03 18:50:46 +0100

    se ha creado el form

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
 
   return (
     <>
+        <h1>TODO LIST</h1>
     <TodoList/>
     </>
   )
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import { useState } from "react";
+
 
 const TodoList = () => {
+
+const [tarea, setTarea] = useState("");
+const [lista, setLista] = useState([])
 
 const handleSubmit = (e) => {
     e.preventDefault();
 }
 
+const handleChange = (e) => setTarea(e.target.value)
+
+const handleClick = () => {
+  if(tarea.trim() === "") return;
+  setLista([...lista, tarea]);
+  setTarea("");
+}
 
   return (
     <>
-    <h1>TODO LIST</h1>
-    <form action="" onSubmit={handleSubmit}>
+   <form action="" onSubmit={handleSubmit}>
         <label>
-            Tarea: <input type="text" name="tarea" />
+            Tarea: <input type="text" value={tarea} placeholder="Escribe una tarea" name="tarea" onChange={handleChange}/>
         </label>
-        <input type="submit" value="Agregar" />
-        
+        <input type="submit" value="Agregar" onClick={handleClick}/>
+
+        <ul>
+          {lista.map((t, index) => (<li key={index}>{t}</li>))}
+        </ul>
+
     </form>
     </>
   )
