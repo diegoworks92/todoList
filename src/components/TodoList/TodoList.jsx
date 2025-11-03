6dbf5f53d56dcc045fd2d47006d627080f1aba79
Diego Reyes <diegoreyes@diegoworks.com>
2025-11-03 19:35:26 +0100

    se agregó el funcionamiento de boton de eliminar y tarea realizada

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Button } from "react-bootstrap";
 
 
 const TodoList = () => {
@@ -8,30 +9,43 @@ const [lista, setLista] = useState([])
 
 const handleSubmit = (e) => {
     e.preventDefault();
+     if(tarea.trim() === "") return;
+  setLista([...lista, {id: Date.now(), texto: tarea, completada: false}]);
+  setTarea("");
 }
 
 const handleChange = (e) => setTarea(e.target.value)
 
-const handleClick = () => {
-  if(tarea.trim() === "") return;
-  setLista([...lista, tarea]);
-  setTarea("");
+
+// Delete por id
+const handleDelete = (id) => {
+  setLista(lista.filter((t) => t.id !== id))
 }
 
+// marcar completada
+ const handleToggle = (id) => {
+    setLista(
+      lista.map((t) =>
+        t.id === id ? { ...t, completada: !t.completada } : t
+      )
+    );
+  };
+
+
   return (
-    <>
-   <form action="" onSubmit={handleSubmit}>
+    <div className="container mt-4">
+   <form onSubmit={handleSubmit} className="mb-3"> 
         <label>
             Tarea: <input type="text" value={tarea} placeholder="Escribe una tarea" name="tarea" onChange={handleChange}/>
         </label>
-        <input type="submit" value="Agregar" onClick={handleClick}/>
+        <Button type="submit" variant="primary">Agregar</Button>
 
         <ul>
-          {lista.map((t, index) => (<li key={index}>{t}</li>))}
+          {lista.map((t, index) => (<li key={index}><p>{t.texto}</p><Button variant="danger" size="sm" onClick={() => handleDelete(t.id)}>Eliminar</Button> <Button variant={t.completada ? "success" : "outline-success"} onClick={() => handleToggle(t.id)}>{t.completada ? "Hecho" : "Ok?"}</Button></li>))}
         </ul>
 
     </form>
-    </>
+    </div>
   )
 }
 
